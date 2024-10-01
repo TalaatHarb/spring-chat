29d462812b3171ffd22a950192109767c131d2b0
mharb <mohamed.harb@cegedim.com>
2024-10-01 17:01:47 +0300

    create basic project structure

---
+++ chat-backend/src/test/java/net/talaatharb/chat/ChatBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package net.talaatharb.chat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
